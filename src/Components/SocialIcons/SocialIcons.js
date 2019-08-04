c5112016ece3dcaf0add2bd102a8babe6bd76e89
The Luciferous <shivamaima619@gmail.com>
2019-08-04 22:18:48 +0530

    defactoring

--- src/Components/SocialIcons/SocialIcons.js
+++ src/Components/SocialIcons/SocialIcons.js
@@ -6,7 +6,6 @@ const SocialIcons = () => {
 		<div className="wrapper">
   			<ul>
 			    <li className="facebook"><i className="fa fa-facebook fa-2x" aria-hidden="true"></i></li>
-			    <li className="twitter"><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></li>
 			    <li className="instagram"><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></li>
 			    <li className="google"><i className="fa fa-google fa-2x" aria-hidden="true"></i></li>
   			</ul>
