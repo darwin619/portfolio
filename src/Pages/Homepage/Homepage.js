a4815dce0c7c74fdc8d6fc48781f2fe975246859
The Luciferous <shivamaima619@gmail.com>
2019-08-09 12:56:11 +0530

    finishing up stuff

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './Homepage.scss';
 import Profile from './Profile.jpg';
 import CustomButton from '../../Components/CustomButton/CustomButton';
@@ -7,50 +7,47 @@ import Thanos from "react-thanos";
 import InfinityGauntlet from "react-thanos-snap";
 import logo from "./HomepageSnap/download.png";
 
-
-
 const Homepage = ({history}) => {
-	const [snap, setSnap] = useState(false);
+	const [snapp, setSnapp] = useState(false);
 		return(
-
+			<Fragment>
 			<div className="homepage">
-			<Header homepage/>
-			<InfinityGauntlet snap={snap}>
-			<div className="homepage-container">
-				<div>
-					<img src={Profile} alt="profile" className="profileimage"/>
+				<div className="content">
+					<Header homepage className="navheader"/>
+					 
+						<div className={`${snapp ? 'rotation' : ''} homepage-container `} >
+							<div>
+								<img src={Profile} alt="profile" className="profileimage"/>
+							</div>
+						<div className="text-box">
+							<h1 className="title">Hi, I'm Shivam Aima.</h1>
+						  	<span className="subtitle">A Frontend Developer</span>
+						 </div>
+						 <div className="buttons-container">
+							 <div className="buttons">
+								 <CustomButton onClick={() => history.push('/skills')} >My Skills</CustomButton>
+								 <CustomButton onClick={() => history.push('/projects')} >Portfolio</CustomButton>
+								 <CustomButton  onClick={() => window.open(``, '_blank') } >Resume</CustomButton>
+								 <CustomButton  onClick={() => window.open(`https://bit.ly/shivam_linkedin`, '_blank') }>Linkedin</CustomButton>
+								 <CustomButton  onClick={() => window.open(`https://bit.ly/shivam_github`, '_blank') } >Github</CustomButton>
+							 </div>
+						 </div>
+						 </div>
+					 
+				 </div>
+
+				 <div className="footer" >
+						<Thanos  className="thanos"
+							onSnap={() => setSnapp(!snapp) }
+							onSnapReverse={() => setSnapp(!snapp) }
+						/>
+						<div className="text" >
+							<h1 className="made">Copyright©2019, ReactJS is</h1>
+							<i className="fa fa-heart-o heart-off" aria-hidden="true"></i>
+					</div>
 				</div>
-			<div className="text-box">
-				<h1 className="title">Hi, I'm Shivam Aima.</h1>
-			  	<span className="subtitle">A Frontend Developer</span>
-			 </div>
-			 <div className="buttons-container">
-			 <div className="buttons">
-			 <CustomButton onClick={() => history.push('/skills')} >My Skills</CustomButton>
-			 <CustomButton onClick={() => history.push('/projects')} >Portfolio</CustomButton>
-			 <CustomButton  onClick={() => window.open(``, '_blank') } >Resume</CustomButton>
-			 <CustomButton  onClick={() => window.open(`https://bit.ly/shivam_linkedin`, '_blank') }>Linkedin</CustomButton>
-			 <CustomButton  onClick={() => window.open(`https://bit.ly/shivam_github`, '_blank') } >Github</CustomButton>
-			 </div>
-			 </div>
-			 </div>
-			 </InfinityGauntlet>
-
-
-			<div className="footer">
-			 <Thanos  className="thanos"
-					  onSnap={() => setSnap(!snap) }
-					  onSnapReverse={() => setSnap(!snap) }
-					/>		
-			 
-			 <div className="text">
-			 			 
-				    <h1 className="made">Copyright©2019, ReactJS is</h1>
-				    <i class="fa fa-heart-o heart-off" aria-hidden="true"></i>
 			</div>
-			</div>
-	
-			</div>
+			</Fragment>
 			);
 }
 
